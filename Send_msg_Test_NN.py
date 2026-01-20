5414f4064277e56597e58fa7aa7c2b5c55d894a8
GuitarBot <guitarbotgatech@gmail.com>
2026-01-20 11:04:04 -0500

    tuning

--- Send_msg_Test_NN.py
+++ Send_msg_Test_NN.py
@@ -22,8 +22,8 @@ def main():
     # pluck_message = gen.scale(duration=0.69,slide_toggle=True, true_dur=0.1)
     # chords_message = [['On', gen.final_dur]]
 
-    # send_osc_message(client, "/Fret", [41, 0.0])
-    send_osc_message(client, "/Dyn", [[40, 40], [127, 0]])
+    send_osc_message(client, "/Fret", [41, 1.0])
+    # send_osc_message(client, "/Dyn", [[41, 40], [127, 0]])
     # send_osc_message(client, "/Chords", chords_message)
     # send_osc_message(client, "/Strum", strum_message)
 
