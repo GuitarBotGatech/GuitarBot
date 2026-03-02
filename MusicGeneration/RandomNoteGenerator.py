5b214345bfdd3f6506f6d42d8c7042633b4d46a6
GuitarBot <rbaker301@gatech.edu>
2026-03-02 12:30:26 -0500

    removed run on import

--- MusicGeneration/RandomNoteGenerator.py
+++ MusicGeneration/RandomNoteGenerator.py
@@ -507,4 +507,4 @@ def polyrhythms_timestamps(rhythms_to_generate, base_notes, measures, bpm):
 # pluck_message = generate_scale_progression(12) # Play the C Major scale x times
 # pluck_message = sequential_Plucks(4)
 #pluck_message = generate_polyrhythms()
-generate_e_major_blues_progression()
+# generate_e_major_blues_progression()
